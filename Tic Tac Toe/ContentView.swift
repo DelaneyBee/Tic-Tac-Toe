665b2726ae6ec1566b9f5992b4a121121db7d0d5
Delaney Blaszinski <Dablasz6@gmail.com>
2024-11-14 09:32:26 -0600

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Delaney Blaszinski on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
