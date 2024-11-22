01f2a105e2a644c911827222f3391d3c79412e9a
Delaney Blaszinski <Dablasz6@gmail.com>
2024-11-22 10:13:34 -0600

    Added check for winner routine

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe").font(.title).bold()
                 .padding(50)
-                .preferredColorScheme(.dark)
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
                 
                 ForEach(0..<9) { index in
@@ -55,6 +56,19 @@ struct ContentView: View {
             }
             ForEach(0..<9) { index in
             }
+        }
+        .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver) {
+            Alert(title: Text(winMessage))
+        }
+        .onChange(of: moves) { oldValue, newValue in
+            checkforWinner()
+        }
+    }
+    private func checkforWinner() {
+        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2] {
+            winMessage = "\(moves[0]) is the winner"
+            gameOver = true
         }
     }
 }
