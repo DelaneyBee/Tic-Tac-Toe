3752352e1c0d4251b034a65e8356bb10d773ec01
Delaney Blaszinski <Dablasz6@gmail.com>
2024-11-22 09:42:37 -0600

    Added rounded blue tiles

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,36 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
     var body: some View {
         VStack {
-            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3), content: {
+            Text("Tic Tac Toe").font(.title).bold()
+                .padding(50)
+                .preferredColorScheme(.dark)
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
+                
                 ForEach(0..<9) { index in
-                    Color.blue
-                        .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
+                    ZStack {
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
                 }
-                ForEach(0..<9) { index in
-                }
-                ForEach(0..<9) { index in
-                }
-                ForEach(0..<9) { index in
-                }
-                ForEach(0..<9) { index in
-                }
-                ForEach(0..<9) { index in
-                }
-                ForEach(0..<9) { index in
-                }
+            }
                 ForEach(0..<9) { index in
                 }
-                ForEach(0..<9) { index in
-                }
-            })
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
+            ForEach(0..<9) { index in
+            }
         }
-        .preferredColorScheme(.dark)
     }
 }
-
 #Preview {
     ContentView()
 }
